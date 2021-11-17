6e52e717dd3ebe5c40159dcc8298bbd9544cc1a1
RomanSopotnytskyi <sopotnytsky7@gmail.com>
2021-11-17 10:39:34 +0200

    homework 5

--- src/homework5/MainHW5.java
+++ src/homework5/MainHW5.java
@@ -6,6 +6,7 @@ public class MainHW5 {
     public static void main(String[] args) {
 
         //Створити ієрархію фігур (мінімум 4 фігури) і оприділити у них методи для розрахунку площі.
+        //Створити клас Child який буде містити фігуру, childs має бути не менше 4 і в кожного з них різні реалізації фігур.
 
         GeometricFigure figure1= new Square(2);
         GeometricFigure figure2= new Triangle(2,2,2);
@@ -13,16 +14,10 @@ public class MainHW5 {
         GeometricFigure figure4= new Rectangle(2,2);
 
 
-        //Створити клас Child який буде містити фігуру, childs має бути не менше 4 і в кожного з них різні реалізації фігур.
-        var child1 = new Child("Bil", figure1);
-        var child2 = new Child("Pit", figure4);
-        var child3 = new Child("Gun", figure2);
-        var child4 = new Child("Max", figure3);
-
         //Написати код який буде виводити назву дитини, її  фігуру і площу цієї фігури.
-        List<Child> children = List.of(child1, child2, child3, child4);
-        for(Child child: children) {
-            child.printInfoAboutChild();
+        List<GeometricFigure> children = List.of(figure1, figure2, figure3, figure4);
+        for(GeometricFigure child: children) {
+            child.printArea();
         }
     }
 }
